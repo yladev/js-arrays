d1d0c92e09a51ccaa78d3f7d3c1bb630df52f1cc
yladev <yanlinnaung881@gmail.com>
2023-05-08 15:37:53 +0630

    feet: update forEach array method

---
+++ arrays-method/forEach.js
@@ -0,0 +1,34 @@
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(consoleItem);
+
+export function consoleItem(item, index, arr) {
+    console.log(item);
+    console.log('a[' + index + '] = ' + item);
+}
+
+// Arrow Function
+export const result = numbers.forEach((item, index, arr) => {
+    console.log('a[' + index + '] = ' + item);
+    console.log(arr);
+});
+
+// loop through the arrays and sum the item
+export let sum = 0;
+numbers.forEach(item => {
+    sum += item;
+})
+console.log(sum);
+
+export const letters = ['a', 'b', 'a', 'b', 'c', 'd', 'a'];
+
+let count = {};
+
+letters.forEach(item => {
+    if(count[item]) {
+        count[item]++;
+    } else {
+        count[item] = 1;
+    }
+});
+console.log(count);
---
+++ index.js
@@ -0,0 +1,4 @@
+import { consoleItem } from "./arrays-method/forEach.js";
+import { result } from "./arrays-method/forEach.js";
+import { sum } from "./arrays-method/forEach.js";
+import { letters } from "./arrays-method/forEach.js";
